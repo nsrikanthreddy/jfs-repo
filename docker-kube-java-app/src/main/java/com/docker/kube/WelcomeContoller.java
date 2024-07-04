1f767000ca3cf965dfe6e1f7a6fac4eb70c889ab
nsrikanthreddy <nallaballesrikanthreddy@gmail.com>
2024-07-04 12:04:08 +0000

    added showCourses endpoint

--- docker-kube-java-app/src/main/java/com/docker/kube/WelcomeContoller.java
+++ docker-kube-java-app/src/main/java/com/docker/kube/WelcomeContoller.java
@@ -4,6 +4,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Arrays;
+import java.util.List;
+
 @RestController
 @RequestMapping("/api/jfstools")
 public class WelcomeContoller {
@@ -12,5 +15,10 @@ public class WelcomeContoller {
 	public String showWelcomeMessage() {
 		
 		return "Hi, Welcome to Java with Docker and Kubernetes.";
+	}
+        @GetMapping("/showCourses")
+	public List<String> showCourses() {
+
+		return Arrays.asList("Git","Docker","Kubernetes","Jenkins");
 	}
 }
